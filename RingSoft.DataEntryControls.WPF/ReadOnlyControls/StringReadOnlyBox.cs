e9f19d4cbfc380e7ef2fe81ac57c1fe824829635
Peter Ringering <peteman316@gmail.com>
2020-09-19 21:09:36 -0600

    Almost done with StringReadOnlyBox.

---
+++ RingSoft.DataEntryControls.WPF/ReadOnlyControls/StringReadOnlyBox.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+
+// ReSharper disable once CheckNamespace
+namespace RingSoft.DataEntryControls.WPF
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:RingSoft.DataEntryControls.WPF.ReadOnlyControls"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:RingSoft.DataEntryControls.WPF.ReadOnlyControls;assembly=RingSoft.DataEntryControls.WPF.ReadOnlyControls"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:StringReadOnlyBox/>
+    ///
+    /// </summary>
+    public class StringReadOnlyBox : ReadOnlyBox
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(StringReadOnlyBox),
+                new FrameworkPropertyMetadata(TextChangedCallback));
+
+        public new string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private static void TextChangedCallback(DependencyObject obj,
+            DependencyPropertyChangedEventArgs args)
+        {
+            var readOnlyBox = (StringReadOnlyBox)obj;
+            readOnlyBox.SetText();
+        }
+
+        static StringReadOnlyBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StringReadOnlyBox), new FrameworkPropertyMetadata(typeof(StringReadOnlyBox)));
+        }
+
+        private void SetText()
+        {
+            base.Text = Text;
+        }
+    }
+}
