a34e3824e754d82b3aaaa07bba5bd4cb5111ed16
Peter Ringering <peteman316@gmail.com>
2020-09-26 16:57:55 -0600

    Working on ProductWindow.

--- RingSoft.DataEntryControls.NorthwindApp/WpfAppStart.cs
+++ RingSoft.DataEntryControls.NorthwindApp/WpfAppStart.cs
@@ -1,6 +1,7 @@
 ﻿using RingSoft.DataEntryControls.NorthwindApp.Library;
 using RingSoft.DbLookup.Controls.WPF;
 using System.Windows;
+using RingSoft.DbLookup.Lookup;
 
 namespace RingSoft.DataEntryControls.NorthwindApp
 {
@@ -41,11 +42,18 @@ namespace RingSoft.DataEntryControls.NorthwindApp
 
         protected override void FinishStartup()
         {
-            //RsDbLookupAppGlobals.EfProcessor.NorthwindLookupContext.LookupAddView += NorthwindLookupContext_LookupView;
-            //RsDbLookupAppGlobals.EfProcessor.MegaDbLookupContext.LookupAddView += MegaDbLookupContextOnLookupView;
+            AppGlobals.LookupContext.LookupAddView += LookupContext_LookupAddView;
 
             _application.MainWindow = _mainWindow;
             _mainWindow.Show();
+        }
+
+        private void LookupContext_LookupAddView(object sender, DbLookup.Lookup.LookupAddViewArgs e)
+        {
+            if (e.LookupData.LookupDefinition.TableDefinition == AppGlobals.LookupContext.Products)
+            {
+                ShowAddOnTheFlyWindow(new ProductWindow(), e);
+            }
         }
 
 
@@ -76,14 +84,14 @@ namespace RingSoft.DataEntryControls.NorthwindApp
         //}
 
 
-        //private void ShowAddOnTheFlyWindow(DbMaintenanceWindow maintenanceWindow, LookupAddViewArgs e)
-        //{
-        //    if (e.OwnerWindow is Window ownerWindow)
-        //        maintenanceWindow.Owner = ownerWindow;
+        private void ShowAddOnTheFlyWindow(DbMaintenanceWindow maintenanceWindow, LookupAddViewArgs e)
+        {
+            if (e.OwnerWindow is Window ownerWindow)
+                maintenanceWindow.Owner = ownerWindow;
 
-        //    maintenanceWindow.ShowInTaskbar = false;
-        //    maintenanceWindow.InitializeFromLookupData(e);
-        //    maintenanceWindow.ShowDialog();
-        //}
+            maintenanceWindow.ShowInTaskbar = false;
+            maintenanceWindow.InitializeFromLookupData(e);
+            maintenanceWindow.ShowDialog();
+        }
     }
 }
