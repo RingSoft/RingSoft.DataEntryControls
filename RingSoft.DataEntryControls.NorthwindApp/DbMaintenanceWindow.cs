28ab927bc725f0340d40062c3ff2027f47b40bcd
Peter Ringering <peteman316@gmail.com>
2023-05-29 14:34:55 -0600

    Working

--- RingSoft.DataEntryControls.NorthwindApp/DbMaintenanceWindow.cs
+++ RingSoft.DataEntryControls.NorthwindApp/DbMaintenanceWindow.cs
@@ -38,20 +38,9 @@ namespace RingSoft.DataEntryControls.NorthwindApp
             Processor.OnValidationFail(fieldDefinition, text, caption);
         }
 
-        public void HandleAutoFillValFail(DbAutoFillMap autoFillMap)
-        {
-            LookupControlsGlobals.HandleValFail(this, autoFillMap);
-        }
-
         public virtual void ResetViewForNewRecord()
         {
         }
-
-        public List<DbAutoFillMap> GetAutoFills()
-        {
-            return LookupControlsGlobals.GetAutoFills(this);
-        }
-
 
         protected override void OnReadOnlyModeSet(bool readOnlyValue)
         {
