bb692ee4e498d65e29e58095053330ba87fe6f26
Peter Ringering <peteman316@gmail.com>
2024-06-26 20:27:18 -0600

    Working

--- RingSoft.DataEntryControls.NorthwindApp/InstallFrom Downloads.bat
+++ RingSoft.DataEntryControls.NorthwindApp/InstallFrom Downloads.bat
@@ -1,2 +1,3 @@
 move C:\Users\petem\Downloads\RingSoft.WPFDataEntryControlsDemoApp.exe "C:\Inno Install Output"
+del C:\Users\petem\source\repos\RingSoft\Nuget\Release\CodeSign\RingSoft.WPFDataEntryControlsDemoApp.exe
 pause
