471f7868f87c0da9cc365f8216ecd9f00c7da726
Peter Ringering <peteman316@gmail.com>
2024-10-08 13:33:41 -0600

    Working

---
+++ RingSoft.DataEntryControls.WPF/BaseUserControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Controls;
+
+namespace RingSoft.DataEntryControls.WPF
+{
+    public class BaseUserControl : UserControl
+    {
+        /// <summary>
+        /// Gets or sets a value whether to set focus to the first editable control when the window first shows.
+        /// </summary>
+        /// <value><c>true</c> if [set focus to first control]; otherwise, <c>false</c>.</value>
+        public bool SetFocusToFirstControl { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [enter to tab].
+        /// </summary>
+        /// <value><c>true</c> if [enter to tab]; otherwise, <c>false</c>.</value>
+        public bool EnterToTab { get; set; }
+
+    }
+}
