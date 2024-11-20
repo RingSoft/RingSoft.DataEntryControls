af94d6f9f47b354bbc595040489db4d624beb810
Peter Ringering <peteman316@gmail.com>
2024-11-20 14:43:38 -0700

    Working

--- RingSoft.DataEntryControls.WPF/EnhancedButton.cs
+++ RingSoft.DataEntryControls.WPF/EnhancedButton.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace RingSoft.DataEntryControls.WPF
 {
@@ -32,13 +34,115 @@ namespace RingSoft.DataEntryControls.WPF
     ///     <MyNamespace:EnhancedButton/>
     ///
     /// </summary>
+    [TemplatePart(Name = "Image", Type = typeof(Image))]
     public class EnhancedButton : Button
     {
+        public static readonly DependencyProperty ImageSourceProperty =
+    DependencyProperty.RegisterAttached(
+        nameof(ImageSource)
+        , typeof(ImageSource)
+        , typeof(EnhancedButton),
+        new FrameworkPropertyMetadata(null, ImageSourceChangedCallback));
+
+        public ImageSource ImageSource
+        {
+            get => (ImageSource)GetValue(ImageSourceProperty);
+            set => SetValue(ImageSourceProperty, value);
+        }
+
+        private static void ImageSourceChangedCallback(DependencyObject obj,
+            DependencyPropertyChangedEventArgs args)
+        {
+            var dbMaintenanceButton = (EnhancedButton)obj;
+            dbMaintenanceButton.SetImage();
+        }
+
+        private static void VisibilityChangedCallback(DependencyObject obj,
+            DependencyPropertyChangedEventArgs args)
+        {
+            var dbMaintenanceButton = (EnhancedButton)obj;
+            var grid = dbMaintenanceButton.GetParentOfType<Grid>();
+            if (grid != null)
+            {
+                if (grid.RowDefinitions.Count <= 1)
+                {
+                    var columnIndex = Grid.GetColumn(dbMaintenanceButton);
+                    var columnDefinition = grid.ColumnDefinitions[columnIndex];
+                    if (columnDefinition != null)
+                    {
+                        switch (dbMaintenanceButton.Visibility)
+                        {
+                            case Visibility.Visible:
+                                columnDefinition.Width = new GridLength(1, GridUnitType.Star);
+                                break;
+                            case Visibility.Hidden:
+                            case Visibility.Collapsed:
+                                columnDefinition.Width = new GridLength(0);
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                }
+
+                if (grid.ColumnDefinitions.Count <= 1)
+                {
+                    var rowIndex = Grid.GetRow(dbMaintenanceButton);
+                    var rowDefinition = grid.RowDefinitions[rowIndex];
+                    if (rowDefinition != null)
+                    {
+                        switch (dbMaintenanceButton.Visibility)
+                        {
+                            case Visibility.Visible:
+                                rowDefinition.Height = new GridLength(1, GridUnitType.Star);
+                                break;
+                            case Visibility.Hidden:
+                            case Visibility.Collapsed:
+                                rowDefinition.Height = new GridLength(0);
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    }
+                }
+            }
+        }
+
+        public Image Image { get; set; }
         static EnhancedButton()
         {
             DefaultStyleKeyProperty.OverrideMetadata(
                 typeof(EnhancedButton)
                 , new FrameworkPropertyMetadata(typeof(EnhancedButton)));
+
+            VisibilityProperty.OverrideMetadata(
+                typeof(EnhancedButton)
+                , new FrameworkPropertyMetadata(VisibilityChangedCallback));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            Image = GetTemplateChild(nameof(Image)) as Image;
+
+            SetImage();
+
+            base.OnApplyTemplate();
+        }
+
+        private void SetImage()
+        {
+            if (Image != null)
+            {
+                if (ImageSource == null)
+                {
+                    Image.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    Image.Source = ImageSource;
+                    Image.Visibility = Visibility.Visible;
+                }
+            }
         }
     }
 }
